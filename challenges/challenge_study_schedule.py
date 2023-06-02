bd4855ab425d861a679d313c2e2e0be35aa82534
marcelsguima <marcelsguima@gmail.com>
2023-06-02 17:30:26 -0300

    Challenge 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(target_time, int):
+        return
+
+    counter = 0
+
+    for start_time, end_time in permanence_period:
+        if not (isinstance(start_time, int) and isinstance(end_time, int)):
+            return
+
+        if start_time <= target_time <= end_time:
+            counter += 1
+
+    return counter
