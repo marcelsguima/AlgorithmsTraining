94c0ba149abfc3dcaa84999be29e281d0257ec57
marcelsguima <marcelsguima@gmail.com>
2023-06-12 21:02:26 -0300

    Challenge 02

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,16 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+
+    message = "bolo de fuba"
+    assert encrypt_message(message, 3) == "lob_abuf ed o"
+    assert encrypt_message(message, 4) == "abuf ed _olob"
+    assert encrypt_message(message, 0) == "abuf ed olob"
+
+    with pytest.raises(TypeError):
+        encrypt_message(80001, 1)
+
+    with pytest.raises(TypeError):
+        encrypt_message(message, "8")
